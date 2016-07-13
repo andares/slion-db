bc8138d4eeeb90cf0e9abc25dbf414fa063472f6
Andares Merigold <andares@outlook.com>
2016-07-13 14:04:32 +0800

    add vo::makeArrary() function

--- src/classes/Slion/DB/Vo.php
+++ src/classes/Slion/DB/Vo.php
@@ -1,12 +1,27 @@
 <?php
 
 namespace Slion\DB;
+use Illuminate\Support\Collection;
 
 /**
  * Description of Vo
  *
  * @author andares
  */
-class Vo {
-    //put your code here
+abstract class Vo extends \Slion\Meta {
+    /**
+     *
+     * @param Collection $collection
+     * @return array
+     */
+    public static function makeArray(Collection $collection): array {
+        $result = [];
+        $class  = get_called_class();
+        foreach ($collection as $row) {
+            $vo = new $class($row->toArray());
+            /* @var $vo self */
+            $result[] = $vo->confirm();
+        }
+        return $result;
+    }
 }
