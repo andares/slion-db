64e6db9fe1b1ba6980e36913f0b3d9a03055815a
Andares Merigold <andares@outlook.com>
2016-08-01 13:00:35 +0800

    remove vo class construct function

--- src/classes/Slion/DB/Vo.php
+++ src/classes/Slion/DB/Vo.php
@@ -2,20 +2,28 @@
 
 namespace Slion\DB;
 use Illuminate\Support\Collection;
+use Slion\Meta;
 
 /**
  * Description of Vo
  *
  * @author andares
  */
-abstract class Vo extends \Slion\Meta {
-    protected static $id_mapping = '';
+abstract class Vo implements \IteratorAggregate, \ArrayAccess, \Serializable, \JsonSerializable {
+    use Meta\Base, Meta\Access, Meta\Serializable, Meta\Json;
 
-    public function __construct(array $data = null) {
-        static::$id_mapping && isset($data[static::$id_mapping]) &&
-            $data['id'] = $data[static::$id_mapping];
+    protected static $fields_mapping = [];
 
-        parent::__construct($data);
+    public function fill($data) {
+        if (!is_array($data) && !is_object($data)) {
+            throw new \InvalidArgumentException("fill data error");
+        }
+
+        foreach ($this->getDefault() as $name => $default) {
+            $field = isset(static::$fields_mapping[$name]) ? static::$fields_mapping[$name] : $name;
+            isset($data[$field]) && $this->$name = $data[$field];
+        }
+        return $this;
     }
 
     /**
