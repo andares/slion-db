0ea68a47d730003ea4e4a71f602221eeca9d9a19
Andares Merigold <andares@outlook.com>
2016-08-08 17:53:24 +0800

    updated

--- src/hooks.php
+++ src/hooks.php
@@ -1,4 +1,6 @@
 <?php
 namespace Slion;
 
-$this->hook->attach(HOOK_BEFORE_RESPONSE, new DB\Vo\Autoload());
+$hook = $container->get('hook');
+/* @var $hook \Slion\Hook */
+$hook->attach(HOOK_BEFORE_RESPONSE, new DB\Vo\Autoload());
