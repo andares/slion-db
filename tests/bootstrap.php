8eb879727fa2da7509782f1a4e5a89e74fd3ab18
Andares Merigold <andares@outlook.com>
2016-08-09 17:42:32 +0800

    update unit test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+use Slim\{App, Container};
+use Slion\{Run};
+
 /**
  * @author andares
  */
@@ -7,5 +10,13 @@ define('IN_TEST', 1);
 $GLOBALS['settings'] = require __DIR__ . '/../vendor/slion/slion/src/settings.php';
 $GLOBALS['settings']['slion_settings']['utils']['config'][0] = __DIR__ . '/../src/config';
 require __DIR__ . '/../vendor/autoload.php';
+
 $run = $GLOBALS['run'];
+$run->select('slion-db')
+
+    ->setup(200, function(string $root, App $app, Container $container, array $settings, Run $r) {
+        // 配置
+        $container->get('config')->addScene('default', "$root/config");
+    }, 'utils setting for unit test');
+
 $run();
